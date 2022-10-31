b5754efc9a2c85ac715001cc5b4d46ac9f3f80e2
csct3434 <csct3434@gmail.com>
2022-10-31 16:15:04 +0900

    View 환경설정

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller     // 웹 어플리케이션에서의 진입점이 되는 곳이 controller
+public class HelloController {
+
+    @GetMapping("hello")    // /hello 요청이 들어오면 해당 메소드가 실행됨
+    public String hello(Model model) {
+        model.addAttribute("data", "hello");
+        return "hello";     // viewResolver가 resources/templates 아래에 있는 hello.html를 찾아서 렌더링함
+    }
+}
